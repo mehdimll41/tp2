97c54a1b9e60148f027a21490b31031b0439491d
mehdimll41 <148041180+mehdimll41@users.noreply.github.com>
2023-10-16 09:39:02 +0200

    Update SimpleTesting.java

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,1 +1,9 @@
+4
+public class SimpleTesting {
+public static void main(String[] args) {
+Student student = new Student("John");
+String name = student.getName();
+System.out.println("Student name is : "+name);
+}
+}
 
